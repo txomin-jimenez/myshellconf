d2aba261c23ede18fee6e30606f0f0ab7507353f
Txomin <tjd@spyro.es>
2015-11-08 10:26:59 +0000

    Added more conf for zshell and vim

---
+++ first-conf.sh
@@ -0,0 +1,3 @@
+cd ~/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+npm install --global pure-prompt
