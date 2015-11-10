c9583ef724b798840d9868b9992d1b1f2fec0298
tjd <txomin.jimenez.dev@gmail.com>
2015-11-10 19:47:49 +0100

    Fixed some modules

--- first-conf.sh
+++ first-conf.sh
@@ -1,3 +1,4 @@
 cd ~/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+cd ~/.oh-my-zsh && git clone https://github.com/bhilburn/powerlevel9k.git themes/powerlevel9k
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-npm install --global pure-prompt
+git pull && git submodule init && git submodule update && git submodule status
